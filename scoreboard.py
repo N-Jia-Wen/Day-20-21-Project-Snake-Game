c100d75c7ed3b55a2bb6d138bbb90d95a194e61d
N-Jia-Wen <157371796+N-Jia-Wen@users.noreply.github.com>
2024-01-31 12:53:52 +0800

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -12,17 +12,24 @@ class ScoreBoard(Turtle):
         self.penup()
         self.teleport(x=0, y=280)
         self.score = 0
+        with open(file="data.txt", mode="r") as data:
+            self.high_score = int(data.read())
         self.show_score()
 
     def show_score(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.show_score()
 
-    def game_over(self):
-        self.teleport(x=0, y=0)
-        self.write(arg="GAME OVER", align=ALIGN, font=FONT)
-        
+# New method added: replaced game_over() with reset()
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open(file="data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score = 0
+        self.show_score()
